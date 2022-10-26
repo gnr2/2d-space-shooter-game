d663ee6b6c0520482c56b4bb45c865306877062c
gnr2 <104433302+gnr2@users.noreply.github.com>
2022-10-27 01:10:47 +0800

    Update Animation.java

--- src/Main/Animation.java
+++ src/Main/Animation.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Main;
 
 import java.awt.Graphics;
@@ -21,6 +17,7 @@ public class Animation {
     private BufferedImage img1;
     private BufferedImage img2;
     private BufferedImage img3;
+    
 //    private BufferedImage img4;
 //    private BufferedImage img5;
 //    private BufferedImage img6;
@@ -35,6 +32,16 @@ public class Animation {
         this.img1 = img1;
         this.img2 = img2;
         this.img3 = img3;
+        
+        frames = 3;
+    }
+    
+    public Animation(int speed, BufferedImage img1,BufferedImage img2){
+        this.speed = speed;
+        this.img1 = img1;
+        this.img2 = img2;
+        
+        frames = 2;
     }
     
     public void runAnimation(){
@@ -46,20 +53,36 @@ public class Animation {
     }
     
     public void nextFrame(){
-        if(count == 0)
-            currentImg = img1;
-            count++;
         
-        if(count == 1)
-            currentImg = img2;
-            count++;
+        switch(frames){
             
-        if(count == 2)
-            currentImg = img3;
-            count++;
-        
-        if(count>frames)
-            count = 0;
+            case 2:
+                
+                if(count == 0)
+                    currentImg = img1;
+                if(count == 1)
+                    currentImg = img2;
+                
+                count++;
+                
+                if(count > frames)
+                    count = 0;
+                break;
+                
+            case 3:
+                if(count == 0)
+                    currentImg = img1;
+                if(count == 1)
+                    currentImg = img2;
+                if(count == 2)
+                    currentImg = img3;
+                
+                count++;
+                
+                if(count > frames)
+                    count = 0;
+                break;
+        }
         
     }
     
